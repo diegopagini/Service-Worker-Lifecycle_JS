19b7379bf0918ba1f32790d9bd224c3a7d6b11f8
Diego Pagini <dq@qbkconsulting.com>
2021-02-15 15:27:42 -0300

    lifecycle

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-
+/** @format */
 
 // Detectar si podemos usar Service Workers
-if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+if (navigator.serviceWorker) {
+	navigator.serviceWorker.register('/sw.js').then((reg) => {
+		// 	setTimeout(() => {
+		// 		reg.sync.register('posteo-gatitos');
+		// 		console.log('Se enviaron fotos de gatitos al server');
+		// 	}, 3000);
+		Notification.requestPermission().then((result) => {
+			console.log(result);
+			reg.showNotification('Hola Mundo!');
+		});
+	});
 }
+
+// fetch('https://reqres.in/api/users')
+// 	.then((resp) => resp.text())
+// 	.then(console.log);
--- sw.js
+++ sw.js
@@ -1,3 +1,47 @@
+/** @format */
 
 // Ciclo de vida del SW
+self.addEventListener('install', (event) => {
+	// Descargar assets
+	// Creamos un cache
+	console.log('SW: Instalando SW!');
 
+	const instalacion = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('SW: Instalaciones terminadas');
+			self.skipWaiting();
+			resolve();
+		}, 100);
+	});
+
+	event.waitUntil(instalacion);
+});
+
+//Cuando el SW toma el control de la app.
+self.addEventListener('activate', (event) => {
+	// Borrar cache viejo
+	console.log('SW: Activo y listo para controlar la app');
+});
+
+// Fetch: Manejo de peticiones HTTP
+self.addEventListener('fetch', (event) => {
+	//Aplicar estrategias del cache.
+	console.log('SW:', event.request.url);
+
+	// if (event.request.url.includes('https://reqres.in/')) {
+	// 	const resp = new Response(`{ok: false, mensaje: 'jajaja'}`);
+	// 	event.respondWith(resp);
+	// }
+});
+
+//Sync: Recuperamos la conexion a internet
+self.addEventListener('sync', (event) => {
+	console.log('Tenemos conexion!');
+	console.log(event);
+	console.log(event.tag);
+});
+
+//Push: Manejar las push notifications
+self.addEventListener('push', (envet) => {
+	console.log('Notificacion recibida');
+});
